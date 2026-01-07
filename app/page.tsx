55b862bd26484aa8f6c26a146c5b62c8359ed938
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2026-01-07 22:35:22 +0300

    add fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ const Home = () => {
   return (
     <div>
       <h1 className="h1-bold text-light-500">Tailwind CSS is FUN!!</h1>
+      <h1 className="h1-bold text-light-500 font-space-grotesk">
+        Tailwind CSS is FUN!!
+      </h1>
     </div>
   );
 };
